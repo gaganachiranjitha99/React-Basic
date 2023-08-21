781c39c917bc6678bdd353af929d2f304ea43e9c
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-08-21 22:34:02 +0530

    updated

--- project/src/App.js
+++ project/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div>
         <Switch>
-                <Route path="/">
+                <Route exact path="/">
                   <Home/>
                 </Route>
 
--- project/src/Pages/Home.js
+++ project/src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
         <h1>Home page</h1>
+        <link to="/About">Go to About page</link>
     </div>
   )
 }
