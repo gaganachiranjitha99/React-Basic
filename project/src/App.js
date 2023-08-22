658cb113c876af97710550119cbecc048c1a7791
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-08-22 23:45:02 +0530

    reactbootstrap components

--- project/src/App.js
+++ project/src/App.js
@@ -3,28 +3,53 @@ import './App.css';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Accordion from 'react-bootstrap/Accordion';
+
 
 //u
 function App() {
+  const [count,setcount]=useState(0);
+  //let count=0;
+  const increment=()=>{
+   // count=+1;
+   setcount(count+1);
+  }
+
+  const decrement=()=>{
+   // count-=1;
+   setcount(count-1);
+  }
   return (
-    <Router>
-      <div>
-        <Switch>
-                <Route exact path="/">
-                  <Home/>
-                </Route>
-
-                <Route path="/About">
-                  <About/>
-                </Route>
-
-                <Route path="/Contact">
-                  <Contact/>
-                </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className='acc'>
+     <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+      
+    </div>
+    
     
   );
 }
